1e648f1f595fbf546803b611a1cd1e4cf315701b
Tomasz Jurkiewicz <tomasz.jurkiewicz@polidea.com>
2017-08-02 16:03:30 +0200

    Split code into modules

--- crawler.go
+++ crawler.go
@@ -2,45 +2,12 @@ package main
 
 import (
   "fmt"
-  "log"
   "os"
   "github.com/dghubble/sling"
   "github.com/streadway/amqp"
 )
 
 const BASE_URL string = "https://www.airbnb.com/api/v1/listings/"
-
-type Params struct {
-  Key string `url:"key,omitempty"`
-}
-
-type ErrorResponse struct {
-  ErrorCode    int    `json:"error_code"`
-  Error        string `json:"error"`
-  ErrorMessage string `json:"error_message"`
-}
-
-type ListingResponse struct {
-  Listing struct {
-    Id             int64   `json:"id"`
-    City           string  `json:"city"`
-    UserId         int64   `json:"user_id"`
-    Latitude       float64 `json:"lat"`
-    Longitude      float64 `json:"lng"`
-    Bathrooms      float64 `json:"bathrooms"`
-    Bedrooms       float64 `json:"bedrooms"`
-    Beds           float64 `json:"beds"`
-    PersonCapacity int     `json:"person_capacity"`
-    CountryCode    string  `json:"country_code"`
-  } `json:"listing"`
-}
-
-func failOnError(err error, key string) {
-  if err != nil {
-    log.Fatalf("%s: %s", key, err)
-  }
-}
-
 
 func ReadListing(id string) {
   params := &Params{Key: os.Getenv("AIRBNB_KEY")}
